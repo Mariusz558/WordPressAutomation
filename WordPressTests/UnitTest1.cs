2aae45ba148513eeef9519858ed5f96098ffc995
Mariusz558 <joga55@poczta.fm>
2016-03-07 23:47:54 +0100

    commit test default branch

--- WordPressTests/UnitTest1.cs
+++ WordPressTests/UnitTest1.cs
@@ -10,7 +10,7 @@ namespace WordPressTests
         [TestMethod]
         public void TestMethod1()
         {
-            var c = new Class1();
+            var c = new Class2();
             c.Go();
         }
     }
