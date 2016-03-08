b050f0e7189198ae1edc608295a99e5651efd7b7
Mariusz558 <joga55@poczta.fm>
2016-03-08 21:05:10 +0100

    First Test - Login Page

---
+++ WordPressAutomation/LoginPage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Firefox;
+
+namespace WordPressAutomation
+{
+    public class LoginPage
+    {
+        public static void GoTo()
+        {
+            var driver = new FirefoxDriver();
+            driver.Navigate().GoToUrl("http://localhost:15662/wp-login.php");
+        }
+
+        public static LoginCommand LoginAs(string userName)
+        {
+            return new LoginCommand(userName);
+        }
+
+        public class LoginCommand
+        {
+            private readonly string userName;
+            public LoginCommand(string userName)
+            {
+                this.userName = userName;
+            }
+        }
+    }
+}
---
+++ WordPressTests/LoginTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WordPressAutomation;
+
+namespace WordPressTests
+{
+    [TestClass]
+    public class LoginTests
+    {
+        [TestMethod]
+        public void Admin_User_Can_Login()
+        {
+            LoginPage.GoTo(); //idz do strony logowania
+            //LoginPage.LoginAs("admin", "password"); zaloguj sie; jedna z metod logowania
+            LoginPage.LoginAs("admin").WithPassword("password").Login();//druga metoda logowania: "fluent style"
+
+            Assert.IsTrue(DashboardPage.IsAt, "Failed to login");//sprawdza czy po zalogowaiu jestesmy na prawidlowej stronie plus komunikat bledu
+
+            
+        }
+    }
+}
